830f1a9590a4694662ad0af75395e6e73a57550d
xav02135 <xav.dx02@gmail.com>
2025-07-22 09:50:31 +0200

    galere

--- exercicephp/bibliotheque.php
+++ exercicephp/bibliotheque.php
@@ -1,6 +1,5 @@
 <?php
-
-        //connexion a la bdd
+ //connexion a la bdd
         session_start();
         $host = 'localhost';
         $dbname = 'bibliotheque';
@@ -10,10 +9,8 @@
 
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connexion réussie !";
-           
-    
-?>
+        echo "Connexion réussie !";    
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,151 +22,8 @@
 <body>
     
  <?php
-
-    if (!isset($_SESSION['utilisateurs'])){
-        echo '<form method="POST">
-        <label>Nom</label>
-        <input type="text" name="nom_utilisateurs">
-        <br>
-        <label>Prenom</label>
-        <input type="text" name="prenom_utilisateurs">
-        <br>
-        <label>Email</label>
-        <input type="email" name="email_">
-        <br>
-        <input type="submit" name="submitConnexion" value="Se connecter">
-        
-        </form><a href="?page=createAccount" ><p>Pas de compte ? créez en un ici! <p></a>';
-    }
-    else {
-        echo'<form method="POST">
-        <input type="submit" name="deconnexion" value="Se deconnecter">
-        <br>
-        
-        </form>';
-
-        echo "Bonjour,  Vous etes connecté.";
-        echo '<br>';
-        echo '<hr>';
-    }
+  
     
-    ?>
-
-
-
-<?php
-
-        if (isset($_POST['submitConnexion'])){
-            $nom = $_POST['nom_utilisateurs'];
-            $prenom = $_POST['prenom_utilisateurs'];
-            $email = $_POST['email_'];
-            
-            $sql = "SELECT * FROM `utilisateurs` WHERE email_ = '$nom'";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
-            $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
-
-            //password_verify pour le hachage inversé
-            if (password_verify($nom, $results[0]["nom_utilisateurs"])) {
-                $_SESSION['utilisateurs'] = [
-                    "nom_utilisateurs" => htmlspecialchars($results[0]["nom_utilisateurs"]),
-                    "prenom_utilisateurs" => htmlspecialchars($results[0]["prenom_utilisateurs"]),
-                    "email_" =>htmlspecialchars($results[0]["email_"]) 
-                ];
-                header ("Location: bibliotheque.php");
-        }
-            else {
-                echo"utilisateur non connecté - Mot de passe incorrect";
-            }
-        }
-
-        if (isset($_POST['deconnexion'])){
-            session_destroy();
-            header("Location: bibliotheque.php");
-        }
-        
-        //create count
-        if (isset($_GET['page']) && $_GET['page'] == 'createAccount'){
-            echo '<form method="POST">
-            <label> Nom</label>
-        <input type="text" name="nom_utilisateurs">
-        
-        <br>
-        <label>Prenom</label>
-        <input type="text" name="prenom_utilisateurs">
-        
-        <br>
-        <label>Adresse mail </label>
-        <input type="text" name="email_">
-        
-       
-        <br>
-        <input type="submit" name="submitNom" value="Creer mon compte">
-        </form>';
-        }
-    ?>
-
-
-
- <hr>
-<?php // fonction de hashage + modification securité (accept les apostrophes)
- if (isset($_POST['submitNom'])) {
-        $Nom = $_POST['nom_utilisateurs'];
-        $Prenom = $_POST['prenom_utilisateurs'];
-        $Mail = $_POST['email_'];
-
-        //fonction de hachage pour le password
-        $hashedPassword =  password_hash($Nom, PASSWORD_DEFAULT);
-
-        $sql = "INSERT INTO `utilisateurs` (`nom_utilisateurs`, `prenom_utilisateurs`, `email_` VALUES (:nom,:prenom,:mail)"; //  accept les apostrophes + securité ou VALUES (?,?,?,?,?)";
-
-        $stmt = $pdo->prepare($sql);
-
-       
-        //ou $stmt->execute([$Nom, $Prenom, $Age, $Mail, $hashedPassword]);
-
-
-        $stmt->execute(([
-            ':nom' => $Nom,
-            ':prenom' => $Prenom,
-            ':mail' => $Mail
-        ]       ));
-    }
-
-
-
-
-    
-
-
-    if (isset($_POST['submitPrenom'])) {
-        $Prenom = $_POST['prenom_utilisateurs'];
-        $sql = "INSERT INTO  `utilisateurs` (`prenom_utilisateurs`) VALUES ('$Prenom')";
-
-        $req = $pdo->prepare($sql);
-        $req->execute();
-    }
-
-
-    if (isset($_POST['submitAdresse_mail-user'])) {
-        $Mail = $_POST['email_'];
-        $sql = "INSERT INTO  `utilisateurs` (`email_`) VALUES ('$Mail')";
-
-        $req = $pdo->prepare($sql);
-        $req->execute();
-    }
-
-
-    if (isset($_POST['submitNom'])) {
-        $Nom = $_POST['nom_utilisateurs'];
-        $sql = "INSERT INTO `utilisateurs` (`nom_utilisateurs`) VALUES ('$Nom')";
-
-        $req = $pdo->prepare($sql);
-        $req->execute();
-    }
-
-
-
 
     ?>
 
